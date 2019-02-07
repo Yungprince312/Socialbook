443cbdac1fc8672e4fe579b50469ba92a438cede
Yungprince312 <aamerlayne@gmail.com>
2019-02-07 15:44:24 -0400

    Added bootstrap modal

--- client/main.js
+++ client/main.js
@@ -2,12 +2,29 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '../lib/collections.js';
 
 Template.profile.events({
   'click .js-like'(event, instance) {
    console.log("You clicked like");
   },
   'click .js-dislike'(event, instance){
-  	alert("Clicked dislike")
-  }
+  	alert("Clicked dislike");
+  },
+});
+
+
+Template.addProfile.events({
+	'click .js-save'(event, instance){
+	var fName = $("#exampleModal input[name='firstname']").val();
+	var lName = $("#exampleModal input[name='lastname']").val();
+	var Photopic = $("#exampleModal input[name='Photopic']").val();
+	console.log("The first name is",fName);
+	console.log("The last name is",lName);
+	console.log("The Photo is",Photopic);
+	 $("#exampleModal input[name='firstname']").val("");
+	 $("#exampleModal input[name='lastname']").val("");
+	 $("#exampleModal input[name='Photopic']").val("");		
+  	$('#exampleModal').modal("hide");
+  },
 });
